81bbdbd88c3dd117522690b76dc0623053fc44d2
Kirill Ishtuganov <ian574miller@gmail.com>
2024-01-27 00:26:49 +0300

    fix Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -8,13 +8,17 @@ public class Prime {
         Engine.startTheGame("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
         for (var i = 0; i < Engine.getNumberOfQuestions(); i++) {
-            int number = (int) (Math.random() * 30) + 3;
+            int number = (int) (Math.random() * 30) + 2;
             Engine.getQuestion(String.valueOf(number), isSimple(number));
         }
         Engine.finishTheGame();
     }
 
-    public static String isSimple(Integer number) {
+    public static String isSimple(int number) {
+        if (number == 2 || number == 4) {
+            return "no";
+        }
+
         for (int i = 2; i < number / 2; i++) {
             if (number % i == 0) {
                 return "no";
